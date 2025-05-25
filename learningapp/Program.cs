f89082cc93afa28d29fb0bc6f1a9cff887ff7b1e
mayuresh483 <mayuresh483@gmail.com>
2025-05-25 15:13:38 +0530

    Code Commit 5

--- learningapp/Program.cs
+++ learningapp/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.FeatureManagement;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Configuration.AddAzureAppConfiguration("Endpoint=https://learningazure-appconfig.azconfig.io;Id=s4F7;Secret=1h0nr3oG5gdGfkWVqowUAHsdhx1SVSOqIaZeOdWQTPhc6fxskLweJQQJ99BEACi5YpzJUGLxAAACAZAC2cpz");
+builder.Configuration.AddAzureAppConfiguration(
+    option =>
+    {
+        option.Connect("Endpoint=https://learningazure-appconfig.azconfig.io;Id=s4F7;Secret=1h0nr3oG5gdGfkWVqowUAHsdhx1SVSOqIaZeOdWQTPhc6fxskLweJQQJ99BEACi5YpzJUGLxAAACAZAC2cpz");
+        option.UseFeatureFlags();
+    }
+);
+builder.Services.AddFeatureManagement();
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
