4a157b667d7ffc806f651c813eaf34d46b79d34a
mayuresh483 <mayuresh483@gmail.com>
2025-05-25 11:58:25 +0530

    Code Commit 2

--- learningapp/Pages/Index.cshtml.cs
+++ learningapp/Pages/Index.cshtml.cs
@@ -18,23 +18,23 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-        string connectionString = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")!;
-        var sqlconnect = new SqlConnection(connectionString);
-        sqlconnect.Open();
+        // string connectionString = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")!;
+        // var sqlconnect = new SqlConnection(connectionString);
+        // sqlconnect.Open();
 
-        var sqlcommand = new SqlCommand(
-            "SELECT CourseId,CourseName,Rating FROM Course;", sqlconnect);
-        using (SqlDataReader sqlDataReader = sqlcommand.ExecuteReader())
-        {
-            while (sqlDataReader.Read())
-            {
-                courses.Add(new Course()
-                {
-                    CourseId = Int32.Parse(sqlDataReader["CourseId"].ToString()),
-                    CourseName = sqlDataReader["CourseName"].ToString(),
-                    Rating=Decimal.Parse(sqlDataReader["Rating"].ToString())
-                });
-            }
-        }
+        // var sqlcommand = new SqlCommand(
+        //     "SELECT CourseId,CourseName,Rating FROM Course;", sqlconnect);
+        // using (SqlDataReader sqlDataReader = sqlcommand.ExecuteReader())
+        // {
+        //     while (sqlDataReader.Read())
+        //     {
+        //         courses.Add(new Course()
+        //         {
+        //             CourseId = Int32.Parse(sqlDataReader["CourseId"].ToString()),
+        //             CourseName = sqlDataReader["CourseName"].ToString(),
+        //             Rating=Decimal.Parse(sqlDataReader["Rating"].ToString())
+        //         });
+        //     }
+        // }
     }
 }
